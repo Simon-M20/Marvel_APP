87eeed301751c8c668e7966797d2b66745ea405f
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2022-12-30 21:47:05 -0400

    Create Side Menu Element

--- src/Components/SideMenu/SideMenu.jsx
+++ src/Components/SideMenu/SideMenu.jsx
@@ -1,13 +1,43 @@
-import "boxicons";
+import { useContext } from "react";
 import MarvelLogo from "../../Assets/marvel-studios-logo.png";
+import "../../Assets/Styles/SideMenu.css";
+import MarvelContext from "../../Context/GlobalContext";
 
 const SideMenu = () => {
+    const { showMenu, setShowMenu } = useContext(MarvelContext);
+
     return (
-        <aside>
-            <figure>
-                <img src={MarvelLogo} alt='Marvel Studios Logo' />
+        <aside className={`${showMenu ? "active" : ""} side_menu`}>
+            <figure className='w-full mb-10 flex flex-col'>
+                <img
+                    className='w-1/3'
+                    src={MarvelLogo}
+                    alt='Marvel Studios Logo'
+                />
+                <figcaption className='flex justify-start items-start pt-4'>
+                    <button
+                        className='cursor-pointer'
+                        onClick={() => setShowMenu(!showMenu)}>
+                        <span className='material-symbols-outlined'>close</span>
+                    </button>
+                </figcaption>
             </figure>
-            <i>⍟</i>
+            <div className='flex justify-end items-center mb-3 cursor-pointer side_menu-actions'>
+                <span className='text-3xl pr-1'>⍟</span>
+                <span>Home</span>
+            </div>
+            <div className='flex justify-end items-center mb-3 cursor-pointer side_menu-actions'>
+                <span className='text-3xl pr-1'>⍟</span>
+                <span>Characters</span>
+            </div>
+            <div className='flex justify-end items-center mb-3 cursor-pointer side_menu-actions'>
+                <span className='text-3xl pr-1'>⍟</span>
+                <span>Comics</span>
+            </div>
+            <div className='flex justify-end items-center mb-3 cursor-pointer side_menu-actions'>
+                <span className='text-3xl pr-1'>⍟</span>
+                <span>Movies</span>
+            </div>
         </aside>
     );
 };
