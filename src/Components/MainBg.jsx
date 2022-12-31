f4f0cba6543c2717906d65231607d1172ee76a5a
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2022-12-30 21:48:04 -0400

    Custom Image Background

---
+++ src/Components/MainBg.jsx
@@ -0,0 +1,15 @@
+import "../Assets/Styles/MainBg.css";
+
+const MainBg = () => {
+    return (
+        <figure className='absolute figure-bg'>
+            <img
+                className='main-image'
+                src='https://www.themoviedb.org/t/p/original/8QmaBWJVxC4NYUGl6z95MSDtLgb.jpg'
+                alt='Jessica Jones'
+            />
+        </figure>
+    );
+};
+
+export default MainBg;
