2b339506b7172f4387a6dc924f23dee4fcbb26d2
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2022-12-30 21:47:40 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 // import logo from "./logo.svg";
+// import { useContext } from "react";
 import "./App.css";
 
-import Header from "./Components/Header";
+// import Header from "./Components/Header";
+// import MainBg from "./Components/MainBg";
+import Homepage from "./Components/SideMenu/Homepage";
+import SideMenu from "./Components/SideMenu/SideMenu";
+// import MarvelContext from "./Context/GlobalContext";
 
 function App() {
+    // const { showMenu } = useContext(MarvelContext);
+
     return (
         <div className='App'>
             {/* <h1 className='text-3xl font-bold underline'>MARVEL!</h1> */}
-            <Header />
+            <SideMenu />
+            <Homepage />
+            {/* <section
+                className={`${showMenu ? "active" : ""} relative main-section`}>
+                <MainBg />
+                <Header />
+            </section> */}
         </div>
     );
 }
