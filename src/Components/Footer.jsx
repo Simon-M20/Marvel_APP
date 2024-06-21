b55a273f17f61a193c74e4dc7a51f28ad9f790b8
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-06-21 19:13:33 -0300

    Create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer>
+            <p>
+                more about <span>jessica jones</span>
+            </p>
+            <button></button>
+        </footer>
+    );
+}
+
+export default Footer;
