f278f05cb8e3ae027fce07b9262fcda2234fde76
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-06-22 19:06:20 -0300

    Update MainInfo.jsx

--- src/Components/Character/MainInfo.jsx
+++ src/Components/Character/MainInfo.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "../../Assets/Styles/MainInfo.css";
+import { useContext } from "react";
+import MarvelContext from "../../Context/GlobalContext";
 
 const MainInfo = () => {
+    const { singleCharacter } = useContext(MarvelContext);
+    // console.log(singleCharacter);
     return (
-        <section className='text-white text-left px-6'>
+        <section className='text-white text-left title-section w-full'>
             <h1 className='text-2xl font-extrabold font-mono uppercase title'>
-                Jessica Jones
+                {singleCharacter.name}
             </h1>
-            <h2 className='seasons'>3 Seasons</h2>
-            <button>
-                <span class='material-symbols-outlined'>play_circle</span>
+            {singleCharacter.series && (
+                <h2 className='seasons text-lg font-thin'>
+                    {singleCharacter.series.available} Seasons
+                </h2>
+            )}
+            <span className='w-10 bg-white mt-1 mb-4 py-0.5 block'></span>
+            <button className='playButton'>
+                <span className='material-symbols-outlined'>play_circle</span>
             </button>
         </section>
     );
