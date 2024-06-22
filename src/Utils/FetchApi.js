3a1fe29273e7d040388ef93d7f8f85e8e9c210d7
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-06-22 19:06:47 -0300

    Create FetchApi.js

---
+++ src/Utils/FetchApi.js
@@ -0,0 +1,69 @@
+import CryptoJS from "crypto-js";
+
+const baseUrl = "https://gateway.marvel.com";
+
+let ts = Number(new Date()); //time stamp
+let publicKey = "8f7ecc878fe2a5fd991a1678ad727b53";
+let privateKey = "e6c8194d027e43f883bdb351a72353e357d03a9a";
+
+let hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
+console.log(hash, ts);
+
+// let url = `http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+
+export const searchCharacter = async (id) => {
+    try {
+        let url = `${baseUrl}/v1/public/characters/${id}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const getCharacters = async () => {
+    try {
+        let url = `${baseUrl}/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        return data.data;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const getCharacterData = async (id) => {
+    try {
+        console.log(id);
+        let url = `${baseUrl}/v1/public/characters/${id}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const getCharacterSeries = async (id) => {
+    try {
+        if (id) {
+            let url = `${baseUrl}/v1/public/characters/${id}/series?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+            const res = await fetch(url);
+            const data = await res.json();
+            return data.data;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getCharacterComics = async (character) => {
+    try {
+        const res = await fetch(`${baseUrl}/${character}/encounters`);
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
