3e39c296ccd8726eaab20cee2f8576a669478ee2
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-06-22 19:06:29 -0300

    Update GlobalContext.js

--- src/Context/GlobalContext.js
+++ src/Context/GlobalContext.js
@@ -1,10 +1,49 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { getCharacters } from "../Utils/FetchApi";
 
 const MarvelContext = createContext();
 
 const MarvelProvider = ({ children }) => {
     const [showMenu, setShowMenu] = useState(false);
-    const data = { showMenu, setShowMenu };
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [allCharacters, setAllCharacters] = useState([]);
+    const [singleCharacter, setSingleCharacter] = useState([]);
+
+    const getAllCharacters = async () => {
+        setLoading(true);
+
+        try {
+            const data = await getCharacters();
+            const promises = data.results.map(async (character, index) => {
+                return character;
+            });
+            const results = await Promise.all(promises);
+            setAllCharacters(results);
+            if (results.length > 0) setSingleCharacter(results[0]);
+        } catch (error) {
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getAllCharacters();
+    }, []);
+
+    const data = {
+        showMenu,
+        setShowMenu,
+        error,
+        setError,
+        loading,
+        setLoading,
+        singleCharacter,
+        allCharacters,
+        setSingleCharacter,
+        getAllCharacters,
+    };
 
     return (
         <MarvelContext.Provider value={data}>{children}</MarvelContext.Provider>
