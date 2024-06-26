866e2eb26ce2760b334693ec5072d9f1a6775dee
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-06-26 14:56:34 -0300

    Create SearchCards.jsx

---
+++ src/Components/Character/SearchCards.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function SearchCards({ name, img, id, onSelect }) {
+    const [selectedCard, setSelectedCard] = useState(null);
+
+    const handleClick = (id) => {
+        setSelectedCard(id);
+        onSelect();
+    };
+
+    return (
+        <section
+            className='relative mb-6 w-1/2 h-full'
+            style={{ maxHeight: "240px" }}>
+            <figure className='gallery-item'>
+                <img
+                    src={`${img.path}.${img.extension}`}
+                    alt={name}
+                    height={"150px"}
+                    className='gallery-image'
+                    onClick={() => handleClick(id)}
+                />
+                <figcaption className='image-title'>{name}</figcaption>
+                <span className='button-wrapper'>
+                    <button
+                        className={`circle-button ${
+                            selectedCard === id ? "selected" : ""
+                        }`}
+                        onClick={() => handleClick(id)}
+                    />
+                </span>
+            </figure>
+        </section>
+    );
+}
+
+export default SearchCards;
