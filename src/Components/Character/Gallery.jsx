8adc475c354906f0de2f8f83b34e35d16e295f4b
Simón Moreno <75553043+Simon-M20@users.noreply.github.com>
2024-06-26 13:15:05 -0300

    Update Gallery.jsx
    - Added ColorRing component from the "react-loader-spinner" library
    
    - Create a new loading state for the component instead of the global loading state
    
    - Added useRef and useCallback methods
    
    - Create new states for pages
    
    - Create function to load more series
    
    - Updated the main function getSeries
    
    - Added a new useEffect to load more series when srolling

--- src/Components/Character/Gallery.jsx
+++ src/Components/Character/Gallery.jsx
@@ -1,29 +1,47 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useRef, useCallback } from "react";
 import "../../Assets/Styles/Gallery.css";
 import MarvelContext from "../../Context/GlobalContext";
 import { getCharacterSeries } from "../../Utils/FetchApi";
+import { ColorRing } from "react-loader-spinner";
 
 function Gallery() {
-    const [selectedSerie, setSelectedSerie] = useState(1);
+    const [selectedSerie, setSelectedSerie] = useState(null);
     const [series, setSeries] = useState([]);
+    const [page, setPage] = useState(0);
+    const [loading, setLoading] = useState(false);
     const { singleCharacter, setError } = useContext(MarvelContext);
+    const galleryRef = useRef(null);
 
     const handleClick = (id) => {
         setSelectedSerie(id);
     };
 
+    const loadMoreSeries = useCallback(async () => {
+        if (loading) return;
+        setLoading(true);
+        try {
+            const data = await getCharacterSeries(singleCharacter.id, page);
+            setSeries((prevSeries) => [...prevSeries, ...data.results]);
+            setPage((prevPage) => prevPage + 1);
+        } catch (error) {
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    }, [loading, singleCharacter.id, page, setError]);
+
     useEffect(() => {
         const getSeries = async () => {
+            setLoading(true);
             try {
-                const data = await getCharacterSeries(singleCharacter.id);
-                const promises = data.results.map(async (series) => {
-                    return series;
-                });
-                const results = await Promise.all(promises);
-                setSeries(results);
-                setSelectedSerie(results[0]?.id);
+                const data = await getCharacterSeries(singleCharacter.id, 0);
+                setSeries(data.results);
+                setSelectedSerie(data.results[0]?.id);
+                setPage(1);
             } catch (error) {
                 setError(true);
+            } finally {
+                setLoading(false);
             }
         };
 
@@ -32,11 +50,38 @@ function Gallery() {
         }
     }, [singleCharacter.id, setError]);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const gallery = galleryRef.current;
+            if (
+                gallery.scrollLeft + gallery.clientWidth >=
+                gallery.scrollWidth - 200
+            ) {
+                loadMoreSeries();
+            }
+        };
+
+        const gallery = galleryRef.current;
+        if (gallery) {
+            gallery.addEventListener("scroll", handleScroll);
+        }
+
+        return () => {
+            if (gallery) {
+                gallery.removeEventListener("scroll", handleScroll);
+            }
+        };
+    }, [loadMoreSeries]);
+
+    console.log(loading);
+
     return (
         <section className='gallery-container'>
-            <section className='gallery'>
-                {series.map((serie) => (
-                    <figure key={serie.id} className='gallery-item'>
+            <section className='gallery' ref={galleryRef}>
+                {series.map((serie, index) => (
+                    <figure
+                        key={`${serie.id}-${index}`}
+                        className='gallery-item'>
                         <img
                             src={`${serie.thumbnail.path}.${serie.thumbnail.extension}`}
                             alt={serie.title}
@@ -56,6 +101,36 @@ function Gallery() {
                         </span>
                     </figure>
                 ))}
+                {loading && (
+                    <span
+                        className='w-full flex items-center justify-center'
+                        style={{ maxHeight: "125px" }}>
+                        <ColorRing
+                            visible={true}
+                            height='80'
+                            width='80'
+                            ariaLabel='color-ring-loading'
+                            wrapperStyle={{}}
+                            wrapperClass='color-ring-wrapper'
+                            colors={[
+                                "#3e0007",
+                                "#560007",
+                                "#6e0006",
+                                "#870006",
+                                "#a00103",
+                            ]}
+                        />
+                        {/* <Audio
+                            height='80'
+                            width='80'
+                            radius='9'
+                            color='green'
+                            ariaLabel='loading'
+                            wrapperStyle
+                            wrapperClass
+                        /> */}
+                    </span>
+                )}
             </section>
             <span className='line'></span>
         </section>
